44395a891ead492a0113f653baf72592ccfe2feb
GHOST <ghostdevbusiness@gmail.com>
2021-01-07 11:57:44 +0000

    chore: updated boilerplate

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,8 +1,1 @@
-<script>
-    import RoutifyIntro from './example/_components/RoutifyIntro.svelte'
-    import { metatags } from '@roxi/routify'
-    metatags.title = 'My Routify app'
-    metatags.description = 'Description coming soon...'
-</script>
-
-<RoutifyIntro />
+<p>Hello World</p>
